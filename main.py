b054ce69a8ce7bdf339285413393379aa93539c8
Andrei Tarnauceanu <andrei_tarnauiceanu@trimble.com>
2019-06-04 17:32:47 +0300

    partial commit

--- main.py
+++ main.py
@@ -12,14 +12,13 @@ def load_data(task):
     )
     for key in data.result.keys():
         task.host[key] = data.result[key]
-    # import pdb; pdb.set_trace()
     r = task.run(task=template_file, template="router.j2", path="")
     task.host["template_config"] = r.result
-    task.run(task=napalm_configure, configuration=task.host["template_config"])
+    task.run(task=napalm_configure, replace=False, configuration=task.host["template_config"])
 
 
 nr = InitNornir(config_file="config.yaml")
 routers = nr.filter(platform="ios")
-
-r = routers.run(load_data)
+nornir = routers.filter(site="nornir")
+r = nornir.run(load_data)
 print_result(r)
--- show.py
+++ show.py
@@ -1,13 +1,12 @@
 from nornir import InitNornir
 from nornir.plugins.tasks.networking import netmiko_send_command
-from nornir.plugins.tasks.networking import napalm_configure
 from nornir.plugins.functions.text import print_result
 
 commands = input("Command: ").split(",")
 for cmd in commands:
     nr = InitNornir(config_file="config.yaml")
     result = nr.run(
-        task=napalm_configure,
+        task=netmiko_send_command,
         command_string=cmd
     )
     print_result(result)
